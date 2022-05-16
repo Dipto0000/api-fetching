801a9dc8cb6eb52aee799cafee198f57d5c76ace
Shahriar Ahmed <dipto2041@gmail.com>
2022-05-16 10:39:33 +0600

    fetched data

---
+++ js/fetch.js
@@ -0,0 +1,38 @@
+/* 
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+.then(response => response.json())
+.then(data => console.log(data)) 
+*/
+
+/* 
+function userLoaded(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(users => displayUsers(users))
+}
+
+function displayUsers(users){
+    const usersUl = document.getElementById('users-ul');
+    for(const user of users){
+        const createUl = document.createElement('li');
+        createUl.innerText = `Name: ${user.name}, Email: ${user.email}`;
+        usersUl.appendChild(createUl)
+    }
+}
+*/
+
+function postLoaded() {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => res.json())
+    .then((posts) => postLoader(posts));
+}
+
+function postLoader(data) {
+  const usersUl = document.getElementById("users-ul");
+  for (const post of data) {
+    console.log(post.title);
+    const li = document.createElement("li");
+    li.innerText = post.title;
+    usersUl.appendChild(li);
+  }
+}
